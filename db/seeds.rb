abfdd0c6b60b94a84ada82d4f910528dd727d982
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-04-11 23:08:44 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+50.times do
+  Book.create(title: Faker::Book.title, author: Faker::Book.author, description: Faker::Hipster.sentence)
+end
+
+100.times do
+  username =   Faker::Color.color_name + "_" + Faker::Hipster.word
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: username )
+end
+
+
+200.times do
+  UserBook.create(user_id: Random.rand(100), book_id: Random.rand(50))
+end
