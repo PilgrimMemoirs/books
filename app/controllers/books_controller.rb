f1ba1e87b0f15eca8999a8846a0cabe9cc4781d3
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-04-11 23:37:29 -0700

    books index, show and create functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,33 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create book_params
+
+    if @book.id != nil
+      flash[:success] = "Book added successfully"
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
+
+
 end
