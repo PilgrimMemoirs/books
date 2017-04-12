49f0d61e5b8e8cce2216e9957e64f3939867f77f
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-04-12 00:59:46 -0700

    require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+ def current_user
+   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+ end
+
+ def require_login
+   if current_user.nil?
+     flash[:error] = "You must be logged in to view this section"
+     redirect_to login_path
+   end
+ end
 end
