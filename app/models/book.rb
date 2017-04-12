a4c3c761136caa50ef1cb21897335755504fcde9
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-04-11 23:08:31 -0700

    added model associations book, user and user_books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  has_many :user_books
+  has_many :users, through: :user_books
 end
--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,2 +1,4 @@
 class UserBook < ApplicationRecord
+  belongs_to :book
+  belongs_to :user
 end
