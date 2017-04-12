b5b457d3339a9c7b45f68ff716ad50b9124d95bb
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-04-11 23:09:07 -0700

    add migration data user, books and user_books

--- db/migrate/20170412053245_create_books.rb
+++ db/migrate/20170412053245_create_books.rb
@@ -1,7 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :books do |t|
-
+      t.string :title
+      t.string :author
+      t.string :description
       t.timestamps
     end
   end
--- db/migrate/20170412053537_create_user_books.rb
+++ db/migrate/20170412053537_create_user_books.rb
@@ -1,7 +1,8 @@
 class CreateUserBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :user_books do |t|
-
+      t.belongs_to :user
+      t.belongs_to :book
       t.timestamps
     end
   end
